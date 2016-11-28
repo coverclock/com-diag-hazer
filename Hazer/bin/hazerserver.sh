62702431710191b1950c578be263c849f8958cad
Chip Overclock <coverclock@diag.com>
2016-11-28 14:53:41 +0000

    [1] Checkpointing.

--- Hazer/bin/hazerserver.sh
+++ Hazer/bin/hazerserver.sh
@@ -3,7 +3,8 @@ export PATH=${PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../sym:
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../lib:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../lib
 GPS=${1:-"/dev/ttyUSB0"}
 GPSSPEED=${2:-"4800"}
-TTY=${3:-"/dev/ttyS0"}
-TTYSPEED=${4:-"115200"}
-sleep 10
-serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | serialtool -D ${TTY} -b ${TTYSPEED} -8 -n -1 -l
+HOST="[::1]"
+PORT=5555
+serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP '
+#serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP ' | socat -u STDIN STDOUT
+#serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP ' | socat -x -u STDIN UDP6-DATAGRAM:${HOST}:${PORT}
