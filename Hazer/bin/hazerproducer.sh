d9aaa41f184a28e21ce5fbeb79cbfb48b10697dc
Chip Overclock <coverclock@diag.com>
2016-11-28 16:59:07 +0000

    [1] Renamed server to producer. [2] Still iterating on socat options.

--- Hazer/bin/hazerproducer.sh
+++ Hazer/bin/hazerproducer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export PATH=${PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../sym:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../bin:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../tst:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../sym:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../bin:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../tst
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../lib:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../lib
-GPS=${1:-"/dev/ttyUSB0"}
-GPSSPEED=${2:-"4800"}
+DEVICE=${1:-"/dev/ttyUSB0"}
+SPEED=${2:-"4800"}
 HOST="[::1]"
 PORT=5555
-serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP '
-#serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP ' | socat -u STDIN STDOUT
-#serialtool -D ${GPS} -b ${GPSSPEED} -8 -1 -n -l | gpstool | grep '^MAP ' | socat -x -u STDIN UDP6-DATAGRAM:${HOST}:${PORT}
+serialtool -D ${DEVICE} -b ${SPEED} -8 -1 -n -l -i | gpstool | grep '^MAP ' | socat -u - TCP6:${HOST}:${PORT}
