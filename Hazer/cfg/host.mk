5334dd1e045cf9beda80162ec80528151791261e
Chip Coverclock <coverclock@diag.com>
2017-01-24 10:08:59 -0700

    [1] First cut at a Makefile.

---
+++ Hazer/cfg/host.mk
@@ -0,0 +1,27 @@
+# Copyright 2017 Digital Aggregates Corporation
+# Licensed under the terms in README.hz
+# author:Chip Overclock
+# mailto:coverclock@diag.com
+# http://www.diag.com/navigation/downloads/Hazer.html
+# "Chip Overclock" is a registered trademark.
+# "Digital Aggregates Corporation" is a registered trademark.
+
+# host: most Linux/GNU systems hosting the native toolchain.
+
+ARCH				:=	x86_64
+OS					:=	linux
+TOOLCHAIN			:=
+KERNELCHAIN			:=
+KERNEL_REV			:=	$(shell uname -r)
+KERNEL_DIR			:=	/lib/modules/$(KERNEL_REV)/build
+# sudo apt-get install linux-headers-$(uname -r)
+CPPARCH				:=	-isystem /usr/src/linux-headers-$(KERNEL_REV)
+CARCH				:=	-rdynamic -fPIC
+CXXARCH				:=	$(CARCH)
+LDARCH				:=	-L$(OUT)/$(LIB_DIR)
+MOARCH				:=	-L$(OUT)/$(LIB_DIR)
+SOARCH				:=
+SOXXARCH			:=	-L$(OUT)/$(LIB_DIR) -l$(PROJECT)
+KERNELARCH			:=
+LDLIBRARIES			:=	-lpthread -lrt -ldl -lm
+LDXXLIBRARIES		:=	$(LDLIBRARIES)
