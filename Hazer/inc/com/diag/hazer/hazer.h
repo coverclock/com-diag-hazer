b9ccb5ece5e8a9180d56740004f0cdd8a6e34141
Chip Coverclock <coverclock@diag.com>
2017-01-24 10:47:12 -0700

    [1] First cut at header file.

---
+++ Hazer/inc/com/diag/hazer/hazer.h
@@ -0,0 +1,36 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_HAZER_
+#define _H_COM_DIAG_HAZER
+
+/**
+ * @file
+ *
+ * Copyright 2017 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Hazer.html<BR>
+ *
+ * This file is part of the Digital Aggregates Corporation Hazer package.
+ * Hazer is a simple C-based parser of the National Marine Electronics
+ * Association (NMEA) strings produced by the USGlobalSat BU-353S4 Global
+ * Positioning System (GPS) device, a tiny little external GPS receiver that
+ * emits NMEA strings over its built-in USB-to-serial adaptor. The BU-353S4
+ * is based on the SiRF Star IV chipset. If you want to futz around with
+ * satellite geolocation, the BU-353S4 is a cheap and easy way to do it.
+ *
+ * REFERENCES
+ *
+ * "BU-353S4 GPS Receiver Data Sheet", BU353S4-DS08212013B, USGLobalSat Inc.,
+ * 2013
+ *
+ * "GP-2106 SiRF Star IV GPS module with antenna", version 0.2, ADH Technology
+ * Co. Ltd., 2010-12-08
+ *
+ * "SiRF Binary Protocol Reference Manual", revision 2.4, 1040-0041, SiRF
+ * Technology, Inc., 2008-11
+ *
+ * "NMEA 0183 Standard for Interfacing Marine Electronic Devices", version 4.10,
+ * NMEA 0183, National Marine Electronics Association, 2012-06
+ */
+
+#endif
