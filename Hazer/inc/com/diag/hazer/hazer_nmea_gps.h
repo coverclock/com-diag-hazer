01b448054ce3aab00b9ea1c8fbc1af57e35af46e
Chip Coverclock <coverclock@diag.com>
2017-01-26 11:17:24 -0700

    [1] Converted naive read function into a state machine. [2] Added some stderr prints in read function for resyncing. [3] Tested.

---
+++ Hazer/inc/com/diag/hazer/hazer_nmea_gps.h
@@ -0,0 +1,49 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_NMEA_GPS_
+#define _H_COM_DIAG_NMEA_GPS_
+
+/**
+ * @file
+ *
+ * Copyright 2017 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock <coverclock@diag.com><BR>
+ * http://www.diag.com/navigation/downloads/Hazer.html<BR>
+ */
+
+/**
+ * NMEA 0183 4.10, 6.1.4, Table 6
+ */
+static const char HAZER_NMEA_TALKER_GPS[]   = "GP";
+
+/**
+ * GP-2106, 2.1, Table 2-1, Global positioning system fixed data
+ */
+static const char HAZER_NMEA_MESSAGE_GGA[]  = "GGA";
+
+/**
+ * GP-2106, 2.1, Table 2-1, Geographic position latitude / longitude
+ */
+static const char HAZER_NMEA_MESSAGE_GLL[]  = "GLL";
+
+/**
+ * GP-2106, 2.1, Table 2-1, GNSS DOP and active satellites
+ */
+static const char HAZER_NMEA_MESSAGE_GSA[]  = "GSA";
+
+/**
+ * GP-2106, 2.1, Table 2-1, GNSS satellites in view
+ */
+static const char HAZER_NMEA_MESSAGE_GSV[]  = "GSV";
+
+/**
+ * GP-2106, 2.1, Table 2-1, Recommended minimum specific GNSS data
+ */
+static const char HAZER_NMEA_MESSAGE_RMC[]  = "RMC";
+
+/**
+ * GP-2106, 2.1, Table 2-1, Course over ground and ground speed
+ */
+static const char HAZER_NMEA_MESSAGE_VTG[]  = "VTG";
+
+#endif
