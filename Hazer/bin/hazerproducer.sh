38a334a738d42b441bbe848b5a2639e7e7e220fe
Chip Coverclock <coverclock@diag.com>
2017-02-08 07:54:49 -0700

    [1] Checkpointing before branching. [2] Will likely NOT use the producer/consumer script solution (latency).

--- Hazer/bin/hazerproducer.sh
+++ Hazer/bin/hazerproducer.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 export PATH=${PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../sym:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../bin:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../tst:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../sym:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../bin:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../tst
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/pi/src/com-diag-diminuto/Diminuto/out/host/bin/../lib:/home/pi/src/com-diag-hazer/Hazer/out/host/bin/../lib
-DEVICE=${1:-"/dev/ttyUSB0"}
-SPEED=${2:-"4800"}
-HOST="[::1]"
-PORT=5555
+
+HOST=${1:-"[::1]"}
+PORT=${2:-"5555"}
+DEVICE=${3:-"/dev/ttyUSB0"}
+SPEED=${4:-"4800"}
+
 serialtool -D ${DEVICE} -b ${SPEED} -8 -1 -n -l -i | gpstool | grep '^MAP ' | socat -u - TCP6:${HOST}:${PORT}
