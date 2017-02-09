5669c166ae77c7c3e77f9a799821bc07f9eb159a
Chip Coverclock <coverclock@diag.com>
2017-02-09 13:12:35 -0700

    [1] First cut at producer/consumer scripts.

---
+++ Hazer/bin/consumer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=${PATH}:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../sym:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../bin:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../tst:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../sym:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../bin:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../tst
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../lib:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../lib
+
+PORT=${1:-"5555"}
+
+exec gpstool -E -6 -P ${PORT}
---
+++ Hazer/bin/producer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=${PATH}:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../sym:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../bin:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../tst:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../sym:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../bin:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../tst
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/src/com-diag-diminuto/Diminuto/out/host/bin/../lib:${HOME}/src/com-diag-hazer/Hazer/out/host/bin/../lib
+
+HOST=${1:-"[::1]"}
+PORT=${2:-"5555"}
+DEVICE=${3:-"/dev/ttyUSB0"}
+SPEED=${4:-"4800"}
+
+exec gpstool -D ${DEVICE} -b ${SPEED} -8 -n -1 -E -6 -A ${HOST} -P ${PORT}
