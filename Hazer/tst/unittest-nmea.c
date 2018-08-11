9a053a510fdae3dbba7bf0f118925bd5fe391a20
Chip Overclock <coverclock@diag.com>
2018-08-11 14:50:56 -0600

    [1] Checkpoint new NMEA unit test. [2] Builds.

---
+++ Hazer/tst/unittest-nmea.c
@@ -0,0 +1,167 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+/**
+ * @file
+ *
+ * Copyright 2018 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Licensed under the terms in README.h<BR>
+ * Chip Overclock (coverclock@diag.com)<BR>
+ * https://github.com/coverclock/com-diag-hazer<BR>
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include "com/diag/hazer/hazer.h"
+#include "com/diag/hazer/yodel.h"
+
+int main(void)
+{
+	{
+		static const char * DATA = "$GNGGA,135627.00,3947.65338,N,10509.20216,W,2,12,0.67,1708.6,M,-21.5,M,,0000*4E\r\n";
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_position_t position = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+
+		strncpy(buffer, DATA, sizeof(buffer));
+		buffer[sizeof(buffer) - 1] = '\0';
+		assert(strcmp(DATA, buffer) == 0);
+
+		length = hazer_length(buffer, sizeof(buffer));
+		assert(length == strlen(buffer));
+
+		count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+		assert(count == 16);
+
+		rc = hazer_parse_gga(&position, vector, count);
+		assert(rc == 0);
+	}
+
+	{
+		static const char * DATA = "$GNRMC,135628.00,A,3947.65337,N,10509.20223,W,0.010,,070818,,,D*74\r\n";
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_position_t position = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+
+		strncpy(buffer, DATA, sizeof(buffer));
+		buffer[sizeof(buffer) - 1] = '\0';
+		assert(strcmp(DATA, buffer) == 0);
+
+		length = hazer_length(buffer, sizeof(buffer));
+		assert(length == strlen(buffer));
+
+		count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+		assert(count == 13);
+
+		rc = hazer_parse_rmc(&position, vector, count);
+		assert(rc == 0);
+	}
+
+	{
+		static const char * DATA = "$GNGLL,3947.65337,N,10509.20223,W,135628.00,A,D*6A\r\n";
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_position_t position = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+
+		strncpy(buffer, DATA, sizeof(buffer));
+		buffer[sizeof(buffer) - 1] = '\0';
+		assert(strcmp(DATA, buffer) == 0);
+
+		length = hazer_length(buffer, sizeof(buffer));
+		assert(length == strlen(buffer));
+
+		count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+		assert(count == 8);
+
+		rc = hazer_parse_gll(&position, vector, count);
+		assert(rc == 0);
+	}
+
+	{
+		static const char * DATA = "$GNVTG,,T,,M,0.021,N,0.040,K,D*3F\r\n";
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_position_t position = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+
+		strncpy(buffer, DATA, sizeof(buffer));
+		buffer[sizeof(buffer) - 1] = '\0';
+		assert(strcmp(DATA, buffer) == 0);
+
+		length = hazer_length(buffer, sizeof(buffer));
+		assert(length == strlen(buffer));
+
+		count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+		assert(count == 10);
+
+		rc = hazer_parse_vtg(&position, vector, count);
+		assert(rc == 0);
+	}
+
+	{
+		static const char * DATA = "$GNGSA,A,3,07,11,15,18,19,13,30,28,51,01,48,17,1.27,0.64,1.10*1C\r\n";
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_active_t active = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+
+		strncpy(buffer, DATA, sizeof(buffer));
+		buffer[sizeof(buffer) - 1] = '\0';
+		assert(strcmp(DATA, buffer) == 0);
+
+		length = hazer_length(buffer, sizeof(buffer));
+		assert(length == strlen(buffer));
+
+		count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+		assert(count == 18);
+
+		rc = hazer_parse_gsa(&active, vector, count);
+		assert(rc == 0);
+	}
+
+	{
+		static const char * DATA[] = {
+			"$GPGSV,4,1,15,01,37,078,36,06,02,184,29,07,28,143,44,08,00,048,22*7A\r\n",
+			"$GPGSV,4,2,15,11,36,059,30,13,36,270,37,15,15,304,28,17,63,226,40*7B\r\n",
+			"$GPGSV,4,3,15,18,24,052,32,19,32,223,36,28,67,020,28,30,59,149,38*77\r\n",
+			"$GPGSV,4,4,15,46,38,215,40,48,36,220,34,51,44,183,45*47\r\n",
+		};
+		hazer_buffer_t buffer = { 0 };
+		hazer_vector_t vector = { 0 };
+		hazer_view_t view = { 0 };
+		ssize_t length = -1;
+		size_t count = 0;
+		int rc = -1;
+		int ii = 0;
+
+		for (ii = 0; ii < (sizeof(DATA) / sizeof(DATA[0])); ++ii) {
+
+			strncpy(buffer, DATA[ii], sizeof(buffer));
+			buffer[sizeof(buffer) - 1] = '\0';
+			assert(strcmp(DATA[ii], buffer) == 0);
+
+			length = hazer_length(buffer, sizeof(buffer));
+			assert(length == strlen(buffer));
+
+			count = hazer_tokenize(vector, sizeof(vector) / sizeof(vector[0]), buffer, length);
+			assert(((ii == 3) && (count == 16)) || (count == 20));
+
+			rc = hazer_parse_gsv(&view, vector, count);
+			assert(rc == 0);
+
+		}
+	}
+}
