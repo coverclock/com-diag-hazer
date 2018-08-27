6137befada002a1b1ed2711e2d2008d57194602d
Chip Overclock <coverclock@diag.com>
2018-08-27 13:48:52 -0600

    [1] Added -W options to BU-353W10 script to adjust GSV and VTG frequencies. [2] Tested.

--- Hazer/fun/bu353w10.sh
+++ Hazer/fun/bu353w10.sh
@@ -4,12 +4,14 @@
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
 
-# This script is specific to the GlobalSat BU-353W10, but
-# because that device uses the same U-Blox 8 chipset as
-# the Navlocate GN-803G (but is easily acquireable via
-# Amazon.com with two-day shipping, unilike the GN which
-# comes from eBay by way of China), this script is the
-# same as the one for that device.
+# This script is specific to the GlobalSat BU-353W10. It is mostly
+# the same as the script for the Navlocate GR-803G (with which it
+# shares the use of the U-blox 8 chipset). But it sends two
+# proprietary sentences to the BU-353W10 to cause it to [1] emit
+# the satellite view (GSV) every second, and [2] emit the VTG
+# sentence containing the course over ground (COG) and speed
+# over ground (SOG) values every second, something the GR-803G
+# does, but which the BU-353W10 does not right out of the box.
 
 PROGRAM=$(basename ${0})
 DEVICE=${1:-"/dev/ttyACM0"}
@@ -20,4 +22,4 @@ RATE=${2:-9600}
 DIR=$(readlink -e $(dirname ${0})/..)/log
 mkdir -p ${DIR}
 
-eval coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 2>> ${DIR}/${PROGRAM}.log
+eval coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 -W '\$PUBX,40,GSV,0,0,0,1,0,0' -W '\$PUBX,40,VTG,0,0,0,1,0,0' 2>> ${DIR}/${PROGRAM}.log
