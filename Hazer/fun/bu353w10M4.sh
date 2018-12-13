5fca8e99a68bcf8ae26b6b5d335140a2e9e46eaa
Chip Overclock <coverclock@diag.com>
2018-12-13 12:55:29 -0700

    [1] Removed "-M 4" from bu353w10F functional test. [2] Added bu353w10M4 function test with "-M 4".

---
+++ Hazer/fun/bu353w10M4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2018 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+
+# This script is specific to the GlobalSat BU-353W10. It is mostly
+# the same as the script for the Navlocate GR-803G (with which it
+# shares the use of the U-blox 8 chipset). But it sends two
+# proprietary sentences to the BU-353W10 to cause it to [1] emit
+# the satellite view (GSV) every second, and [2] emit the VTG
+# sentence containing the course over ground (COG) and speed
+# over ground (SOG) values every second, something the GR-803G
+# does, but which the BU-353W10 does not right out of the box.
+# This script also redirects the standard error stream to the
+# system log.
+
+PROGRAM=$(basename ${0})
+DEVICE=${1:-"/dev/ttyACM0"}
+RATE=${2:-9600}
+
+. $(readlink -e $(dirname ${0})/../bin)/setup
+
+. $(readlink -e $(dirname ${0})/../fun)/ubx8
+
+OPTIONS=""
+for OPTION in ${COMMANDS}; do
+    OPTIONS="${OPTIONS} -W ${OPTION}"
+done
+
+eval coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 -M 4 ${OPTIONS} 2> >(log -S -N ${PROGRAM})
