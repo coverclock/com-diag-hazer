4b84dafd574a4be3dacc5752e9fc460707e633e4
Chip Overclock <coverclock@diag.com>
2019-03-27 09:20:52 -0600

    [1] Tested GPIO volatile changes in gpstool using spare Uputronics board. [1.5] Added GPIO18 PPS option to uputronics script.

--- Hazer/fun/uputronics.sh
+++ Hazer/fun/uputronics.sh
@@ -3,18 +3,17 @@
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
+# N.B. Probably needs to be ruy as root for GPIO access.
 
 PROGRAM=$(basename ${0})
 DEVICE=${1:-"/dev/ttyAMA0"}
 RATE=${2:-9600}
+ONEPPS=${3:-18}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-. $(readlink -e $(dirname ${0})/../fun)/ubx8
+stty sane
 
-OPTIONS=""
-for OPTION in ${COMMANDS}; do
-    OPTIONS="${OPTIONS} -W ${OPTION}"
-done
+coreable pintool -p ${ONEPPS} -n 2> >(log -S -N ${PROGRAM}) || true
 
-eval coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 ${OPTIONS} 2> >(log -S -N ${PROGRAM})
+coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -I ${ONEPPS} -E -t 10 2> >(log -S -N ${PROGRAM})
