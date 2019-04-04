eb67c3bc798c0a5ab07491e4ee09ac878d54896b
Chip Overclock <coverclock@diag.com>
2019-04-04 15:34:46 -0600

    [1] First cut at base and mobile configurations with TMODEs (NOT WORKING).

--- Hazer/fun/simplertk2bbase.sh
+++ Hazer/fun/simplertk2bbase.sh
@@ -12,7 +12,14 @@ RATE=${2:-9600}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
+# UBX-CFG-VALGET [ 8] V0 RAM 0 0 CFG-NMEA-PROTVER : 41 = "4.10"
+# UBX-CFG-VALSET [ 9] V0 RAM 0 0 CFG-TMODE-MODE SURVEY_IN
+# UBX-CFG-VALSET [12] V0 RAM 0 0 CFG_TMODE_SVIN_MIN_DUR 86400 (seconds)
+# UBX-CFG-VALSET [12] V0 RAM 0 0 CFG_TMODE_SVIN_ACC_LIMIT 1000 (0.1mm)
+
 exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 \
-    -U '\xb5\x62\x06\x8b\x08\x00\0x00\0x00\x00\x00\x20\x93\x00\x01' \
-    -U '\xb5\x62\x06\x8b\x08\x00\0x00\0x00\x00\x00\x01\x00\x93\x20' \
+    -U '\xb5\x62\x06\x8b\x08\x00\x00\x00\x00\x00\x01\x00\x93\x20' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x00\x00\x00\x01\x00\x03\x20\x01' \
+    -U '\xb5\x62\x06\x8a\x0c\x00\x00\x00\x00\x00\x10\x00\x03\x40\x80\x51\x01\x00' \
+    -U '\xb5\x62\x06\x8a\x0c\x00\x00\x00\x00\x00\x11\x00\x03\x40\xe8\x03\x00\x00' \
 	2> >(log -S -N ${PROGRAM})
--- Hazer/fun/simplertk2brover.sh
+++ Hazer/fun/simplertk2brover.sh
@@ -12,5 +12,10 @@ RATE=${2:-9600}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 \
-	2> >(log -S -N ${PROGRAM})
+# UBX-CFG-VALGET [ 8] V0 RAM 0 0 CFG-NMEA-PROTVER : 41 = "4.10"
+# UBX-CFG-VALSET [ 9] V0 RAM 0 0 CFG-TMODE-MODE DISABLED
+
+exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 \
+    -U '\xb5\x62\x06\x8b\x08\x00\x00\x00\x00\x00\x01\x00\x93\x20' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x00\x00\x00\x01\x00\x03\x20\x00' \
+    2> >(log -S -N ${PROGRAM})
