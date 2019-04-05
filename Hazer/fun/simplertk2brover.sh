411a9caf77fcf5407a9568afb6b65104b846c0eb
Chip Overclock <coverclock@diag.com>
2019-04-05 14:14:55 -0600

    [1] First cut at UART 2.

--- Hazer/fun/simplertk2brover.sh
+++ Hazer/fun/simplertk2brover.sh
@@ -12,10 +12,14 @@ RATE=${2:-9600}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-# UBX-CFG-VALGET [ 8] V0 RAM 0 0 CFG-NMEA-PROTVER : 41 = "4.10"
-# UBX-CFG-VALSET [ 9] V0 RAM 0 0 CFG-TMODE-MODE DISABLED
+# UBX-MON-VER [0]
+# UBX-CFG-VALGET [8] V0 RAM 0 0 CFG-NMEA-PROTVER
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-MODE DISABLED
+# UBX-CFG-VALGET [9] V0 RAM 0 0 CFG-TMODE-MODE
 
 exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 \
+    -W '\xb5\x62\x0a\x04\x00\x00' \
     -U '\xb5\x62\x06\x8b\x08\x00\x00\x00\x00\x00\x01\x00\x93\x20' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x00\x00\x00\x01\x00\x03\x20\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x01\x00\x03\x20\x00' \
+    -U '\xb5\x62\x06\x8b\x08\x00\x00\x00\x00\x00\x01\x00\x03\x20' \
     2> >(log -S -N ${PROGRAM})
