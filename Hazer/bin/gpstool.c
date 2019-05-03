1bb437ec25d814197b001764e9dfce2d0dac3ab1
Chip Overclock <coverclock@diag.com>
2019-05-03 08:25:26 -0600

    [1] Removed musterious -M PRN help comment. [2] Fix -U help comment. [3] TESTED.

--- Hazer/bin/gpstool.c
+++ Hazer/bin/gpstool.c
@@ -1412,7 +1412,7 @@ int main(int argc, char * argv[])
             verbose = !0;
             break;
         case '?':
-            fprintf(errfp, "usage: %s [ -d ] [ -u ] [ -v ] [ -V ] [ -X ] [ -M PRN ] [ -D DEVICE [ -b BPS ] [ -7 | -8 ] [ -e | -o | -n ] [ -1 | -2 ] [ -l | -m ] [ -h ] [ -s ] | -S FILE ] [ -I PIN ] [ -c ] [ -p PIN ] [ -W STRING ... ] [ -U STRING ... ] [ -R ] [ -E ] [ -F ] [ -H HEADLESS ] [ -A ADDRESS ] [ -P PORT ] [ -O ] [ -L LOG ] [ -t SECONDS ] [ -C ]\n", Program);
+            fprintf(errfp, "usage: %s [ -d ] [ -u ] [ -v ] [ -V ] [ -X ] [ -D DEVICE [ -b BPS ] [ -7 | -8 ] [ -e | -o | -n ] [ -1 | -2 ] [ -l | -m ] [ -h ] [ -s ] | -S FILE ] [ -I PIN ] [ -c ] [ -p PIN ] [ -W STRING ... ] [ -U STRING ... ] [ -R ] [ -E ] [ -F ] [ -H HEADLESS ] [ -A ADDRESS ] [ -P PORT ] [ -O ] [ -L LOG ] [ -t SECONDS ] [ -C ]\n", Program);
             fprintf(errfp, "       -1          Use one stop bit for DEVICE.\n");
             fprintf(errfp, "       -2          Use two stop bits for DEVICE.\n");
             fprintf(errfp, "       -4          Use IPv4 for ADDRESS, PORT.\n");
@@ -1431,7 +1431,7 @@ int main(int argc, char * argv[])
             fprintf(errfp, "       -P PORT     Send to or receive from PORT.\n");
             fprintf(errfp, "       -R          Print a report on standard output.\n");
             fprintf(errfp, "       -S SOURCE   Use SOURCE file for input.\n");
-            fprintf(errfp, "       -U STRING   Collapse STRING, append checksum, write to DEVICE, expect ACK.\n");
+            fprintf(errfp, "       -U STRING   Like -W except expect UBX ACK or NAK response.\n");
             fprintf(errfp, "       -U ''       Exit when this empty STRING is processed.\n");
             fprintf(errfp, "       -V          Print release, vintage, and revision on standard output.\n");
             fprintf(errfp, "       -W STRING   Collapse STRING, append checksum, write to DEVICE.\n");
