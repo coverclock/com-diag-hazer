4621916370610ab80910bc709b32474075d09a6c
Chip Overclock <coverclock@diag.com>
2019-06-07 11:50:18 -0600

    [1] First cut at new base and rover scripts (UNTESTED).

--- Hazer/bin/rover.sh
+++ Hazer/bin/rover.sh
@@ -6,8 +6,9 @@
 # Configure and run the Ardusimple SimpleRTK2B as a mobile Rover.
 
 PROGRAM=$(basename ${0})
-DEVICE=${1:-"/dev/ttyACM0"}
-RATE=${2:-9600}
+ROUTER=${1:-"localhost"}
+DEVICE=${2:-"/dev/ttyACM0"}
+RATE=${3:-115200}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
@@ -15,29 +16,19 @@ LOG=$(readlink -e $(dirname ${0})/..)/log
 mkdir -p ${LOG}
 
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-MODE DISABLED
-# UBX-CFG-VALSET [12] V0 RAM 0 0 CFG-UART2-BAUDRATE 38400
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-STOPBITS 1 (1)
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-DATABITS 0 (8)
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-PARITY 0 (none)
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-ENABLED 1
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2INPROT-RTCM3X 1
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2OUTPROT-RTCM3X 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-ENABLED 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-USBINPROT-RTCM3X 1
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-USBOUTPROT-RTCM3X 0
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-MSGOUT-UBX_RXM_RTCM_USB 1
 # UBX-CFG-MSG [3] UBX-NAV-HPPOSLLH 1
 
-gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 \
+exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 \
+    -Y ${ROUTER}:2101 -y 20 \
+    -F -H ${LOG}/${PROGRAM}.out -t 10 \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x01\x00\x03\x20\x00' \
-    -U '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x01\x00\x53\x40\x00\x96\x00\x00' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x02\x00\x53\x20\x01' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x03\x00\x53\x20\x00' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x53\x20\x00' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x05\x00\x53\x10\x01' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x75\x10\x01' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x76\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x05\x00\x53\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x77\x10\x01' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x78\x10\x00' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x6b\x02\x91\x20\x01' \
     -U '\xb5\x62\x06\x01\x03\x00\x01\x14\x01' \
-    -W '' \
-     2> >(log -S -N ${PROGRAM}) || exit 1
-
-exec gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -F -H ${LOG}/${PROGRAM} -t 10 \
-    1> /dev/tty 2> >(log -S -N ${PROGRAM})
+    1> /dev/tty 2> ${LOG}/${PROGRAM}.err
