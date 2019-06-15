c37675dee4f093d60fdb0256196a9b36a7c40f33
Chip Overclock <coverclock@diag.com>
2019-06-15 17:53:08 -0600

    [1] Trying blacklisting rndis_Host module to support LTE modems on base, rover.

--- Hazer/bin/novatelusb620l.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright 2019 Digital Aggregates Corporation, Colorado USA
-# Licensed unter the terms in LICENSE.txt
-# Chip Overclock <coverclock@diag.com>
-# https://github.com/coverclock/com-diag-hazer
-# Novatel Generic MiFi USB620L v1410p9020
-rmmod rndis_host || true
-/usr/sbin/usb_modeswitch -v 0x1410 -p 0x9020 -u 2 && dhclient eth1
