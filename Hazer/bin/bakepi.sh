20f6651e853e98dfc0f335e256f95fbe5a4154ca
Chip Overclock <coverclock@diag.com>
2019-07-27 10:17:33 -0600

    [1] Minor tweaks to variable name.

--- Hazer/bin/bakepi.sh
+++ Hazer/bin/bakepi.sh
@@ -8,8 +8,8 @@ PROGRAM=$(basename ${0})
 DELAY=${1:-"3"}
 
 while true; do
-	DATE=$(date -u +%Y-%m-%dT%H:%M:%S)
+	TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S)
 	TEMPERATURE=$(/opt/vc/bin/vcgencmd measure_temp | sed 's/^.*=//')
-	echo ${PROGRAM} ${DATE} ${TEMPERATURE}
+	echo ${PROGRAM} ${TIMESTAMP}Z ${TEMPERATURE}
 	sleep ${DELAY}
 done
