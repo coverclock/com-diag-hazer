9c4ea82dfbd14e8a1f71cddea9676dc205859948
Chip Overclock <coverclock@diag.com>
2019-07-30 11:45:29 -0600

    [1] Highly experimental PAWS algorithm implemented. UNTESTED. BUILDS.

--- Hazer/bin/common.h
+++ Hazer/bin/common.h
@@ -11,6 +11,16 @@
  * https://github.com/coverclock/com-diag-hazer<BR>
  *
  * This file is part of the Digital Aggregates Corporation Hazer package.
+ *
+ * This file is not in the inc or src directory because Hazer has a rule
+ * about not creating a dependency to Diminuto in the resulting archive or
+ * shared object.
+ *
+ * It turns out to be remarkably difficult to solve the sequence number
+ * wrap (roll over) problem for the general case. This code borrows from
+ * other applications without implementing the full blown Protection Against
+ * Wrapped Sequence numbers (PAWS) algorithm used by TCP as described in
+ * RFC1323.
  */
 
 #include <arpa/inet.h>
@@ -78,33 +88,36 @@ typedef struct DatagramBuffer {
 #define DATAGRAM_BUFFER_INITIALIZER  { { 0, }, { { '\0', } }, }
 
 /**
- * Check to see if this datagram came out of order (it's okay if there are gaps
- * in the sequence, we expect that).
- * @param sequencep points to the expected sequence counter.
+ * Check to see if this datagram came out of order.
+ * @param expectedp points to the expected sequence number.
  * @param header points to the datagram header.
  * @param length is the total number of received bytes including the header.
  * @param outoforderp points to the Out Of Order counter.
  * @param missingp points to the Missing counter.
  * @return the size of the actual payload of the buffer or <0 if out of order.
  */
-static inline ssize_t validate_datagram(datagram_sequence_t * sequencep, datagram_header_t * header, ssize_t length, unsigned int * outoforderp, unsigned int * missingp)
+static ssize_t validate_datagram(datagram_sequence_t * expectedp, datagram_header_t * header, ssize_t length, unsigned int * outoforderp, unsigned int * missingp)
 {
 	ssize_t result = -1;
-	datagram_sequence_t sequence = 0;
+	datagram_sequence_t actual = 0;
+	datagram_sequence_t gap = 0;
+	static const datagram_sequence_t THRESHOLD = ((datagram_sequence_t)1) << ((sizeof(datagram_sequence_t) * 8) - 1);
 
-	sequence = ntohl(header->sequence);
-	if (sequence < *sequencep) {
-		*outoforderp += 1;
-	} else {
-		if (*sequencep == 0) {
-			/* Do nothing. */
-		} else if (sequence == *sequencep) {
-			/* Do nothing. */
-		} else {
-			*missingp += sequence - *sequencep - 1;
-		}
-		*sequencep = sequence + 1;
+	// (EXPECTED < ACTUAL) if (0 < (ACTUAL - EXPECTED) < THRESHOLD)
+
+	actual = ntohl(header->sequence);
+	if (actual == *expectedp) {
+		*expectedp = *expectedp + 1;
 		result = length - sizeof(datagram_header_t);
+	} else {
+		gap = actual - *expectedp;
+		if (gap < THRESHOLD) {
+			*missingp += gap;
+			*expectedp = actual + 1;
+			result = length - sizeof(datagram_header_t);
+		} else {
+			*outoforderp += 1;
+		}
 	}
 
 	return result;
@@ -112,14 +125,14 @@ static inline ssize_t validate_datagram(datagram_sequence_t * sequencep, datagra
 
 /**
  * Generate a sequence number and store it in the sequence field of the
- * datagram and update the expected sequence counter.
+ * datagram and update the expected sequence number.
  * @param buffer points to the datagram buffer.
- * @param sequencep points to the expected sequence counter.
+ * @param expectedp points to the expected sequence number.
  */
-static inline void stamp_datagram(datagram_header_t * buffer, datagram_sequence_t * sequencep)
+static void stamp_datagram(datagram_header_t * buffer, datagram_sequence_t * expectedp)
 {
-	buffer->sequence = htonl(*sequencep);
-	*sequencep += 1;
+	buffer->sequence = htonl(*expectedp);
+	*expectedp += 1;
 }
 
 #endif
