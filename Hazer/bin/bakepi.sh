b2e969f9739b97cc9c8a75a7ae43d3c50b15c686
Chip Overclock <coverclock@diag.com>
2019-09-05 13:04:00 -0600

    [1] Added hostname to bakepi output.

--- Hazer/bin/bakepi.sh
+++ Hazer/bin/bakepi.sh
@@ -5,12 +5,13 @@
 # https://github.com/coverclock/com-diag-hazer
 
 PROGRAM=$(basename ${0})
+HOSTNAME=$(hostname)
 DELAY=${1:-"3"}
 
 while true; do
 	DEGREE=$'\u00B0'
 	TIMESTAMP=$(date -u +%Y-%m-%dT%H:%M:%S)
 	TEMPERATURE=$(/opt/vc/bin/vcgencmd measure_temp | sed "s/'/${DEGREE}/; s/^.*=//")
-	echo ${PROGRAM} ${TIMESTAMP}Z ${TEMPERATURE}
+	echo ${PROGRAM} ${TIMESTAMP}Z ${TEMPERATURE} ${HOSTNAME}
 	sleep ${DELAY}
 done
