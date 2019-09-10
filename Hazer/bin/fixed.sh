7762ef54ca53c0e7e5d06097be8ff01f25f41b92
Chip Overclock <coverclock@diag.com>
2019-09-10 13:49:45 -0600

    [1] Checkpoint fixed script while debugging.

--- Hazer/bin/fixed.sh
+++ Hazer/bin/fixed.sh
@@ -18,6 +18,10 @@ RATE=${4:-230400}
 
 export COM_DIAG_DIMINUTO_LOG_MASK=0xfe
 
+cp /dev/null ${LOG}/${PROGRAM}.err
+
+test -f ${FILE} || exit 1
+
 exec 0<${FILE}
 
 read -r CFG_TMODE_LAT
@@ -27,28 +31,28 @@ read -r CFG_TMODE_LONHP
 read -r CFG_TMODE_HEIGHT
 read -r CFG_TMODE_HEIGHTHP
 
-log -N ${PROGRAM} -i CFG_TMODE_LAT="\"${CFG_TMODE_LAT}\""
-log -N ${PROGRAM} -i CFG_TMODE_LATHP="\"${CFG_TMODE_LATHP}\""
-log -N ${PROGRAM} -i CFG_TMODE_LON="\"${CFG_TMODE_LON}\""
-log -N ${PROGRAM} -i CFG_TMODE_LONHP="\"${CFG_TMODE_LONHP}\""
-log -N ${PROGRAM} -i CFG_TMODE_HEIGHT="\"${CFG_TMODE_HEIGHT}\""
-log -N ${PROGRAM} -i CFG_TMODE_HEIGHTHP="\"${CFG_TMODE_HEIGHTHP}\""
+log -N ${PROGRAM} -i CFG_TMODE_LAT="\"${CFG_TMODE_LAT}\"" 2>> ${LOG}/${PROGRAM}.err
+log -N ${PROGRAM} -i CFG_TMODE_LATHP="\"${CFG_TMODE_LATHP}\"" 2>> ${LOG}/${PROGRAM}.err
+log -N ${PROGRAM} -i CFG_TMODE_LON="\"${CFG_TMODE_LON}\"" 2>> ${LOG}/${PROGRAM}.err
+log -N ${PROGRAM} -i CFG_TMODE_LONHP="\"${CFG_TMODE_LONHP}\"" 2>> ${LOG}/${PROGRAM}.err
+log -N ${PROGRAM} -i CFG_TMODE_HEIGHT="\"${CFG_TMODE_HEIGHT}\"" 2>> ${LOG}/${PROGRAM}.err
+log -N ${PROGRAM} -i CFG_TMODE_HEIGHTHP="\"${CFG_TMODE_HEIGHTHP}\"" 2>> ${LOG}/${PROGRAM}.err
 
-test -z "${CFG_TMODE_LAT}" && exit 1
-test -z "${CFG_TMODE_LATHP}" && exit 1
-test -z "${CFG_TMODE_LON}" && exit 1
-test -z "${CFG_TMODE_LONHP}" && exit 1
-test -z "${CFG_TMODE_HEIGHT}" && exit 1
-test -z "${CFG_TMODE_HEIGHTHP}" && exit 1
+test -z "${CFG_TMODE_LAT}" && exit 2
+test -z "${CFG_TMODE_LATHP}" && exit 2
+test -z "${CFG_TMODE_LON}" && exit 2
+test -z "${CFG_TMODE_LONHP}" && exit 2
+test -z "${CFG_TMODE_HEIGHT}" && exit 2
+test -z "${CFG_TMODE_HEIGHTHP}" && exit 2
 
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-MODE FIXED
-# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-POS_TYPE LLH
 # UBX-CFG-VALSET [12] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_LAT (read)
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_LATHP (read)
 # UBX-CFG-VALSET [12] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_LON (read)
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_LONHP (read)
 # UBX-CFG-VALSET [12] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_HEIGHT (read)
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-CFG_TMODE_HEIGHTHP (read)
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-MODE FIXED
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-TMODE-POS_TYPE LLH
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-MSGOUT-RTCM_3X_TYPE1005_USB 1
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-MSGOUT-RTCM_3X_TYPE1074_USB 1
 # UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-MSGOUT-RTCM_3X_TYPE1084_USB 1
@@ -64,14 +68,14 @@ test -z "${CFG_TMODE_HEIGHTHP}" && exit 1
 exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 \
     -G ${ROUTER} -g 4 \
     -F -H ${LOG}/${PROGRAM}.out -t 10 \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x01\x00\x03\x20\x02' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x02\x00\x03\x20\x01' \
     -U "${CFG_TMODE_LAT}" \
     -U "${CFG_TMODE_LATHP}" \
     -U "${CFG_TMODE_LON}" \
     -U "${CFG_TMODE_LONHP}" \
     -U "${CFG_TMODE_HEIGHT}" \
     -U "${CFG_TMODE_HEIGHTHP}" \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x01\x00\x03\x20\x02' \
-    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x02\x00\x03\x20\x01' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\xc0\x02\x91\x20\x01' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x61\x03\x91\x20\x01' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x66\x03\x91\x20\x01' \
@@ -82,5 +86,5 @@ exec coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x78\x10\x01' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x8b\x00\x91\x20\x01' \
     -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x05\x00\x53\x10\x00' \
-    -U '\xb5\x62\x06\x01\x03\x00\x01\x14\x01'
-    < /dev/null 1> /dev/null 2> ${LOG}/${PROGRAM}.err
+    -U '\xb5\x62\x06\x01\x03\x00\x01\x14\x01' \
+    < /dev/null 1> /dev/null 2>> ${LOG}/${PROGRAM}.err
