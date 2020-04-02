5d7861144973415b8412cd54b8de1700fb66dbf3
Chip Overclock <coverclock@diag.com>
2020-04-02 08:16:11 -0600

    [1] Updated copyright. [2] Added dgnss functional test script to save some typing. [3] TESTED.

---
+++ Hazer/fun/dgnss.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright 2020 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+base localhost:tumbleweed & sleep 5; exec peruse base out
