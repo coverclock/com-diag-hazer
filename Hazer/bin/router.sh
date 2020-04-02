522c0eb7a5b71cd80e2f049734c7f9963e59c694
Chip Overclock <coverclock@diag.com>
2020-04-02 13:40:24 -0600

    [1] Fixed router script to use canonical out/TARGET/tmp directory.

--- Hazer/bin/router.sh
+++ Hazer/bin/router.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Copyright 2019 Digital Aggregates Corporation, Colorado, USA
+# Copyright 2019-2020 Digital Aggregates Corporation, Colorado, USA
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
 
+SAVDIR=${COM_DIAG_HAZER_SAVDIR:-$(readlink -e $(dirname ${0}))/../tmp}
+mkdir -p ${SAVDIR}
+
 PROGRAM=$(basename ${0})
 ROUTER=${1:-":tumbleweed"}
-ERRFIL=${2-"./${PROGRAM}.err"}
+ERRFIL=${2-"${SAVDIR}/${PROGRAM}.err"}
 
 cp /dev/null ${ERRFIL}
 exec 2>>${ERRFIL}
