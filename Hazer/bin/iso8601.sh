10426de74673e5c291e04bac9e86ba0ccc054a19
Chip Overclock <coverclock@diag.com>
2020-04-24 09:39:53 -0600

    [1] Added examples and nanoseconds to iso8601 script. TESTED.

--- Hazer/bin/iso8601.sh
+++ Hazer/bin/iso8601.sh
@@ -4,5 +4,12 @@
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
 # Converts the number of seconds since the UNIX epoch to an ISO8601 timestamp.
+# Examples
+# $ iso8601 1587742431.047610056
+# 2020-04-24T15:33:51.047610056+00:00
+# $ iso8601 1587742431.000000000
+# 2020-04-24T15:33:51.000000000+00:00
+# $ iso8601 1587742431
+# 2020-04-24T15:33:51.000000000+00:00
 
-date -d "@${1:-0}" -u '+%Y-%m-%dT%H:%M:%S+00:00'
+date -d "@${1:-0}" -u '+%Y-%m-%dT%H:%M:%S.%N+00:00'
