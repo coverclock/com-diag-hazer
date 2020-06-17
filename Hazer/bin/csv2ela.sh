bc3d649cd1864a3bebbc933d2e6eee9718ccdef2
Chip Overclock <coverclock@diag.com>
2020-06-17 12:59:18 -0600

    [1] Adding GE KML output file. TESTED [2] Adding new csv2ela script (elapsed time). TESTED

---
+++ Hazer/bin/csv2ela.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2020 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+# Meters CSV entries from stdin to stdout based
+# on the interarrival time between successive
+# lines.
+#
+
+PREVIOUS=0
+ELAPSED=0.0
+
+while read NAM NUM CLK TIM LAT LON HOR MSL WGS VER SOG COG; do
+
+    if [[ "${NUM}" == "OBSERVATION," ]]; then
+    	echo ${NAM} ${NUM} ${CLK} ${TIM} ${LAT} ${LON} ${HOR} ${MSL} ${WGS} ${VER} ${SOG} ${COG}, ELAPSED
+        continue
+    fi
+
+    CURRENT=${CLK%,}
+    if [[ "${PREVIOUS}" != "0" ]]; then
+        ELAPSED=$(echo "print ${CURRENT} - ${PREVIOUS}" | bc)
+    fi
+    PREVIOUS=${CURRENT}
+
+    echo ${NAM} ${NUM} ${CLK} ${TIM} ${LAT} ${LON} ${HOR} ${MSL} ${WGS} ${VER} ${SOG} ${COG}, ${ELAPSED}
+
+done
+
+exit 0
