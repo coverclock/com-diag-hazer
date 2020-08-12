60f589124bce7b88feeeb3741b7d2623b26c00b3
Chip Overclock <coverclock@diag.com>
2020-08-12 14:09:09 -0600

    [1] Added approximate compass points.

--- Hazer/bin/csv2out.sh
+++ Hazer/bin/csv2out.sh
@@ -37,11 +37,46 @@ while read NAM NUM FIX SYS CLK TIM LAT LON HAC MSL GEO VAC SOG COG ROL PIT YAW R
 	*) SYSTEM="OTHER";;
 	esac
 
-	CLOCK=$(date -d "@${CLK%,}" -u '+%Y-%m-%dT%H:%M:%S.%N+00:00,')
+	TIME=$(date -d "@${TIM%,}" -u '+%Y-%m-%dT%H:%M:%S')
 
-	TIME=$(date -d "@${TIM%,}" -u '+%Y-%m-%dT%H:%M:%S.%N+00:00,')
+	COG=${COG%.*}
+	if [[ ${COG} -lt 11 ]]; then
+		COMPASS="N"
+	elif [[ ${COG} -lt 34 ]]; then
+		COMPASS="NNE"
+	elif [[ ${COG} -lt 56 ]]; then
+		COMPASS="NE"
+	elif [[ ${COG} -lt 79 ]]; then
+		COMPASS="ENE"
+	elif [[ ${COG} -lt 101 ]]; then
+		COMPASS="E"
+	elif [[ ${COG} -lt 124 ]]; then
+		COMPASS="ESE"
+	elif [[ ${COG} -lt 146 ]]; then
+		COMPASS="SE"
+	elif [[ ${COG} -lt 169 ]]; then
+		COMPASS="SSE"
+	elif [[ ${COG} -lt 191 ]]; then
+		COMPASS="S"
+	elif [[ ${COG} -lt 213 ]]; then
+		COMPASS="SSW"
+	elif [[ ${COG} -lt 236 ]]; then
+		COMPASS="SW"
+	elif [[ ${COG} -lt 259 ]]; then
+		COMPASS="WSW"
+	elif [[ ${COG} -lt 281 ]]; then
+		COMPASS="W"
+	elif [[ ${COG} -lt 304 ]]; then
+		COMPASS="WNW"
+	elif [[ ${COG} -lt 326 ]]; then
+		COMPASS="NW"
+	elif [[ ${COG} -lt 349 ]]; then
+		COMPASS="NNW"
+	else
+		COMPASS="?"
+	fi
 
-	echo ${NUM%,} ${TYPE} ${SYSTEM} ${CLOCK} ${TIME} ${POSITION} ${MSL%,}m ${GEO%,}m ${SOG%,}kn ${COG%,}${DEG} ${ROL%,}${DEG} ${PIT%,}${DEG} ${YAW%,}${DEG}
+	echo "[" ${NUM%,} ${TYPE} ${SYSTEM} "] [" ${TIME} "] [" ${POSITION} "] [" ${MSL%,}m ${GEO%,}m "] [" ${SOG%,}kn "] [" ${COG}${DEG} ${COMPASS} "] ["  ${ROL%,}${DEG} ${PIT%,}${DEG} ${YAW%,}${DEG} "]"
 
 done
 
