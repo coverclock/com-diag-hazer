867f63a595095ebf064a1dfec8784346508cd086
Chip Overclock <coverclock@diag.com>
2020-08-27 13:20:25 -0600

    [1] Fixed copyright ocmment in compasstool.

--- Hazer/bin/compasstool.c
+++ Hazer/bin/compasstool.c
@@ -2,7 +2,7 @@
 /**
  * @file
  *
- * Copyright 2019-2020 Digital Aggregates Corporation, Colorado, USA<BR>
+ * Copyright 2020 Digital Aggregates Corporation, Colorado, USA<BR>
  * Licensed under the terms in LICENSE.txt<BR>
  * Chip Overclock <coverclock@diag.com><BR>
  * https://github.com/coverclock/com-diag-hazer<BR>
