c8d60aecb320b1bc642f461724a80c57395cdb63
Chip Overclock <coverclock@diag.com>
2020-09-06 08:24:41 -0600

    [1] Added NUM field to csv2dat output. TESTED

--- Hazer/bin/csv2dat.sh
+++ Hazer/bin/csv2dat.sh
@@ -3,13 +3,15 @@
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
-# Similar to csv2dat but IMO better for post-processing.
+# Similar to csv2out but IMO better for post-processing analysis.
 
 while read NAM NUM FIX SYS SAT CLK TIM LAT LON HAC MSL GEO VAC SOG COG ROL PIT YAW RAC PAC YAC OBS MAC; do
 
 	if [[ "${NUM}" == "NUM," ]]; then
 		continue
 	fi
+
+	NUM=${NUM%,}
 
 	SYSTEM=$(csvsys2str ${SYS})
 
@@ -36,7 +38,7 @@ while read NAM NUM FIX SYS SAT CLK TIM LAT LON HAC MSL GEO VAC SOG COG ROL PIT Y
 	PIT=${PIT%.*}
 	YAW=${YAW%.*}
 
-	printf "%-2s %-2s %-2s | %-20s | %-20s | %19s, %19s | %5sm | %4skn | %3s° %-3s | %3s°, %3s°, %3s°\n" "${SYSTEM}" "${SAT}" "${TYPE}" "${CLOCK}" "${TIME}" "${LATITUDE}" "${LONGITUDE}" "${MSL}" "${SOG}" "${COG}" "${COMPASS}" "${ROL}" "${PIT}" "${YAW}" 
+	printf "%-2s %-2s %-2s | %6s | %-20s | %-20s | %19s, %19s | %5sm | %4skn | %3s° %-3s | %3s°, %3s°, %3s°\n" "${SYSTEM}" "${SAT}" "${TYPE}" "${NUM}" "${CLOCK}" "${TIME}" "${LATITUDE}" "${LONGITUDE}" "${MSL}" "${SOG}" "${COG}" "${COMPASS}" "${ROL}" "${PIT}" "${YAW}" 
 
 done
 
