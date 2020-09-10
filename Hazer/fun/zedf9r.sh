e3e80e83e6c7ea0e276b085c186261c44a088b3b
Chip Overclock <coverclock@diag.com>
2020-09-10 12:00:44 -0600

    [1] Added functional test script for UBX-ZED-F9R. TESTED [2] Updated README with F9R information (a rover with an IMU).

---
+++ Hazer/fun/zedf9r.sh
@@ -0,0 +1,57 @@
+
+#!/bin/bash
+# Copyright 2020 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+# Configure and run the U-blox ZED-UBX-F9R as a mobile Rover receiving
+# corrections from a stationary Base and saving high precision solutions
+# to a CSV file.
+
+SAVDIR=${COM_DIAG_HAZER_SAVDIR:-$(readlink -e $(dirname ${0})/..)/tmp}
+mkdir -p ${SAVDIR}
+
+PROGRAM=$(basename ${0})
+ROUTER=${1:-"tumbleweed:tumbleweed"}
+DEVICE=${2:-"/dev/ttyACM0"}
+RATE=${3:-230400}
+ERRFIL=${4-"${SAVDIR}/${PROGRAM}.err"}
+OUTFIL=${5-"${SAVDIR}/${PROGRAM}.out"}
+CSVFIL=${6-"${SAVDIR}/${PROGRAM}.csv"}
+PIDFIL=${7-"${SAVDIR}/${PROGRAM}.pid"}
+
+mkdir -p $(dirname ${ERRFIL})
+mkdir -p $(dirname ${OUTFIL})
+mkdir -p $(dirname ${CSVFIL})
+mkdir -p $(dirname ${PIDFIL})
+
+cp /dev/null ${ERRFIL}
+exec 2>>${ERRFIL}
+
+. $(readlink -e $(dirname ${0})/../bin)/setup
+
+export COM_DIAG_DIMINUTO_LOG_MASK=0xfe
+
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-I2C-ENABLED 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-SPI-ENABLED 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART1-ENABLED 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-UART2-ENABLED 0
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-USBINPROT-RTCM3X 1
+# UBX-CFG-VALSET [9] V0 RAM 0 0 CFG-MSGOUT-UBX_RXM_RTCM_USB 1
+# UBX-CFG-MSG [3] UBX-NAV-HPPOSLLH 1
+
+exec coreable gpstool \
+    -H ${OUTFIL} -t 10 \
+    -T ${CSVFIL} \
+    -O ${PIDFIL} \
+    -D ${DEVICE} -b ${RATE} -8 -n -1 \
+    -Y ${ROUTER} -y 20 \
+    -x \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x03\x00\x51\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x06\x00\x64\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x05\x00\x52\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x05\x00\x53\x10\x00' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x04\x00\x77\x10\x01' \
+    -U '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x6b\x02\x91\x20\x01' \
+    -U '\xb5\x62\x06\x01\x03\x00\x01\x14\x01' \
+    < /dev/null 1> /dev/null
