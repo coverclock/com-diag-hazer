cb35ea85ab95bba84c82b7d20081a0889f2e18d7
Chip Overclock <coverclock@diag.com>
2020-10-23 22:33:34 +0000

    [1] Rolled back gr701w and makerfocus scripts from exporting PPS but made new scripts that do so. UNTESTED

--- Hazer/fun/gr701wpps.sh
+++ Hazer/fun/gr701wpps.sh
@@ -8,11 +8,17 @@
 # device that is based on a Ublox 7 receiver. The GR-701W is
 # unusual amongst GPS devices that have a USB interface in that
 # it exports the 1PPS signal via data carrier detect (DCD), which
-# is the reason for the "-c" option below.
+# is the reason for the "-c" option below. This script also
+# configures the gpstool to forward the 1PPS signal by strobing a
+# GPIO pin, the "-p" option. In this case, it uses GPIO pin 20 on
+# a hardware test fixture I fabricated. I run this script on a
+# Raspberry Pi as user "pi", which has access to the GPIO pins
+# by virtue of being in group "gpio".
 
 PROGRAM=$(basename ${0})
 DEVICE=${1:-"/dev/ttyUSB0"}
 RATE=${2:-9600}
+STROBE=${3:-"-20"}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
@@ -21,4 +27,4 @@ mkdir -p ${DIR}
 
 export COM_DIAG_DIMINUTO_LOG_MASK=0xfe
 
-coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -c -E -t 10 2>> ${DIR}/${PROGRAM}.err
+coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -c  -p ${STROBE} -E -t 10 2>> ${DIR}/${PROGRAM}.err
--- Hazer/fun/makerfocuspps.sh
+++ Hazer/fun/makerfocuspps.sh
@@ -7,12 +7,19 @@
 # This script tests the MakerFocus USB-Port-GPS, a USB GPS
 # device that is based on a QuecTel L80-R receiver. The MakerFocus
 # is unusual amongst GPS devices that have a USB interface in that
-# it exports the 1PPS signal via via its own GPIO pin, although
-# this script doesn't make use of that feature.
+# it exports the 1PPS signal via via its own GPIO pin, which
+# is the reason for the "-I" option below. This script also
+# configures the gpstool to forward the 1PPS signal by strobing a
+# GPIO pin, the "-p" option. In this case, it uses GPIO pin 16 on
+# a hardware test fixture I fabricated. I run this script on a
+# Raspberry Pi as user "pi", which has access to the GPIO pins
+# by virtue of being in group "gpio".
 
 PROGRAM=$(basename ${0})
 DEVICE=${1:-"/dev/ttyUSB0"}
 RATE=${2:-9600}
+ONEPPS=${3:-18}
+STROBE=${4:-16}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
@@ -21,4 +28,4 @@ mkdir -p ${LOG}
 
 export COM_DIAG_DIMINUTO_LOG_MASK=0xfe
 
-coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -E -t 10 2>> ${LOG}/${PROGRAM}.err
+coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -I ${ONEPPS} -p ${STROBE} -E -t 10 2>> ${LOG}/${PROGRAM}.err
