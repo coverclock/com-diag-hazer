be747c73eb31098cb535b3cfff8cccfb795fdc74
Chip Overclock <coverclock@diag.com>
2021-02-03 09:22:50 -0700

    [1] Slightly improved output format.

--- Hazer/tst/unittest-csv2dgm.sh
+++ Hazer/tst/unittest-csv2dgm.sh
@@ -24,75 +24,107 @@ OUTPUT_YAML="NAM: neon\nNUM: 2\nTIM: 1599145249\nLAT: 39.7943071\nLON: -105.1533
 
 OUTPUT_XML="<?xml version=\"1.0\" encoding=\"UTF-8\" ?><NAM>neon</NAM><NUM>2</NUM><TIM>1599145249</TIM><LAT>39.7943071</LAT><LON>-105.1533805</LON><MSL>1710.300</MSL><LBL>2020-09-03T15:00:49Z</LBL>\n"
 
+echo "**********"
+echo "DEFAULT"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_DEFAULT}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F -)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "CSV"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_CSV}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -c)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "HTML"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_HTML}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -h)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "JSON"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_JSON}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -j)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "QUERY"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_QUERY}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -q)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "VARIABLES"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_VARIABLES}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -v)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "YAML"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_YAML}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -y)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
+echo "**********"
+echo "XML"
+echo "**********"
 EXPECTED="$(echo -e -n "${OUTPUT_XML}")"
 ACTUAL="$(echo -e -n "${INPUT}" | csv2dgm -F - -x)"
+echo "${INPUT}"
+echo "${EXPECTED}"
+echo "${ACTUAL}"
 if [[ "${EXPECTED}" != "${ACTUAL}" ]]; then
-    echo "${INPUT}"
-    echo "${EXPECTED}"
-    echo "${ACTUAL}"
+    echo "FAILED!" 1>&2
     XC=1
 fi
 
