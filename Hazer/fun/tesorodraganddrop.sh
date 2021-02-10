9b7227a2b8b52feb7d1977b91c6e38c60fbcb447
Chip Overclock <coverclock@diag.com>
2021-02-10 10:50:05 -0700

    [1] Checkpoint.

---
+++ Hazer/fun/tesorodraganddrop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2021 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+
+
+PROGRAM=$(basename ${0})
+INPUT=${1:-$(readlink -e $(dirname ${0})/../../../dat/yodel)/20200917/vehicle.csv}
+OUTPUT=${2:-${HOME}/Desktop/Observations/Observation.json}
+
+. $(readlink -e $(dirname ${0})/../bin)/setup
+
+mkdir -p $(dirname ${OUTPUT})
+
+rm -f ${OUTPUT}
+
+csvmeter < ${INPUT} | csv2dgm -F ${OUTPUT} -j
