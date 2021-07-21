f4d3b71928d530d106593abf28b94f2e12e0f02a
Chip Overclock <coverclock@diag.com>
2021-07-21 09:52:23 -0600

    [1] First cut at csvsample. UNTESTED [2] Fixed comments in csvdataset. UNTESTED

--- Hazer/bin/csvsample.sh
+++ Hazer/bin/csvsample.sh
@@ -3,22 +3,20 @@
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
-# Filter that reads CSV and outputs a JSON array to visualize a polyline.
-# Because the array has to be imported and stored entirely in memory,
-# a modulo value can be used to only select a sample of the incoming CSV.
-# The default is modulo 1 (every sample) but for example modulo 10 would
-# select every tenth sample. Regardless of the modulo, the first and last
-# sample are selected so that the starting and ending points are included.
-# To conserve memory, points that have no change in latitude and longitude
+# Filter that reads CSV in a manner similar to csvdataset.sh but instead
+# emitting JSON, emits the sampled CSV.  The default is modulo 1
+# (every sample) but for example modulo 10 would select every tenth
+# sample. Regardless of the modulo, the first and last sample are
+# selected so that the starting and ending points are included. To
+# conserve memory, points that have no change in latitude and longitude
 # are skipped.
 
-# usage: csvdataset [ MODULO ] < CSVFILE > JSONFILE
-# default: csvdataset 1 < CSVFILE > JSONFILE
+# usage: csv2sample [ MODULO ] < CSVFILE > JSONFILE
+# default: csvsample 1 < CSVFILE > JSONFILE
 
 PROGRAM=$(basename ${0})
 MODULO=${1:-1}
 
-INITIALIZED=""
 PRIOR=0
 SEQUENCE=0
 LATITUDE=""
@@ -28,9 +26,10 @@ OLDLONGITUDE=""
 
 while read NAM NUM FIX SYS SAT CLK TIM LAT LON HAC MSL GEO VAC SOG COG ROL PIT YAW RAC PAC YAC OBS MAC; do
 
-	# Skip headings.
+	# Do headings.
 
 	if [[ "${NUM}" == "NUM," ]]; then
+		echo $NAM, $NUM, $FIX, $SYS, $SAT, $CLK, $TIM, $LAT, $LON, $HAC, $MSL, $GEO, $VAC, $SOG, $COG, $ROL, $PIT, $YAW, $RAC, $PAC, $YAC, $OBS, $MAC
 		continue
 	fi
 
@@ -60,21 +59,9 @@ while read NAM NUM FIX SYS SAT CLK TIM LAT LON HAC MSL GEO VAC SOG COG ROL PIT Y
 
 	REMAINDER=$((${SEQUENCE} % ${MODULO}))
 	if [[ ${REMAINDER} -eq 0 ]]; then
-
-		if [[ -z "${INITIALIZED}" ]]; then
-			echo '{'
-			echo '  "PATH": [ '
-			echo -n '      '
-			INITIALIZED=Y
-		else
-			echo -n '    , '
-		fi
-
-		echo '[' "${LATITUDE}, ${LONGITUDE}" ']'
-
+		echo $NAM, $NUM, $FIX, $SYS, $SAT, $CLK, $TIM, $LAT, $LON, $HAC, $MSL, $GEO, $VAC, $SOG, $COG, $ROL, $PIT, $YAW, $RAC, $PAC, $YAC, $OBS, $MAC
 		LATITUDE=""
 		LONGITUDE=""
-
 	fi
 	SEQUENCE=$((${SEQUENCE} + 1))
 
@@ -85,12 +72,8 @@ if [[ -z "${LATITUDE}" ]]; then
 elif [[ -z "${LONGITUDE}" ]]; then
 	:
 else
-	echo '    , [' "${LATITUDE}, ${LONGITUDE}" ']'
-fi
-
-if [[ ${SEQUENCE} -gt 0 ]]; then
-	echo '  ]'
-	echo '}'
+	echo $NAM, $NUM, $FIX, $SYS, $SAT, $CLK, $TIM, $LAT, $LON, $HAC, $MSL, $GEO, $VAC, $SOG, $COG, $ROL, $PIT, $YAW, $RAC, $PAC, $YAC, $OBS, $MAC
 fi
 
 exit 0
+
