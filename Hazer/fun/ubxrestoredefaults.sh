5044a760912e224208ebefdcba8d7263aaf54c8a
Chip Overclock <coverclock@diag.com>
2021-08-22 08:23:10 -0600

    [1] Added scripts to configure BU353W10 for PUBX output.

---
+++ Hazer/fun/ubxrestoredefaults.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2021 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+
+PROCESS=$$
+PROGRAM=$(basename -s .sh ${0})
+DEVICE=${1:-"/dev/ttyACM0"}
+RATE=${2:-115200}
+INTERVAL=${3:-1}
+WRITE=${4:-1}
+
+. $(readlink -e $(dirname ${0})/../../com-diag-hazer/Hazer/out/host/bin)/setup
+
+exec coreable gpstool \
+	-D ${DEVICE} -b ${RATE} -8 -n -1 -m -i ${INTERVAL} \
+	-w ${WRITE} \
+	-U '\xb5\x62\x06\x09\x0d\x00\x1e\x1f\x00\x00\x00\x00\x00\x00\x1e\x1f\x00\x00\x01' \
+	-R -v
