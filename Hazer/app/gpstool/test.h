e3fadf73b21f108dc5e66319a901b0b57d7c8a0f
Chip Overclock <coverclock@diag.com>
2021-11-03 09:34:36 -0600

    [1] Big OOPS! Forgot to add new header file.

---
+++ Hazer/app/gpstool/test.h
@@ -0,0 +1,24 @@
+/* vi: set ts=4 expandtab shiftwidth=4: */
+#ifndef _H_COM_DIAG_HAZER_GPSTOOL_TEST_
+#define _H_COM_DIAG_HAZER_GPSTOOL_TEST_
+
+/**
+ * @file
+ * @copyright Copyright 2021 Digital Aggregates Corporation, Colorado, USA.
+ * @note Licensed under the terms in LICENSE.txt.
+ * @brief This defines the gpstool embedded test symbols.
+ * @author Chip Overclock <mailto:coverclock@diag.com>
+ * @see Hazer <https://github.com/coverclock/com-diag-hazer>
+ * @details
+ * This header file defines the embedded optional test infrastructure.
+ */
+
+#if 0
+#define TEST_EXPIRATION (0x1)
+#endif
+
+#if 0
+#define TEST_ERROR (0x2)
+#endif
+
+#endif
