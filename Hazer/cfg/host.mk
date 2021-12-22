5f3f34e1760276b673093925a9604b926414899f
Chip Overclock <coverclock@diag.com>
2021-12-22 16:59:46 -0700

    [1] Adapted Diminuto's code to determine ARCH. TESTED

--- Hazer/cfg/host.mk
+++ Hazer/cfg/host.mk
@@ -6,11 +6,13 @@
 # http://www.diag.com/navigation/downloads/Hazer.html
 # "Chip Overclock" is a registered trademark.
 # "Digital Aggregates Corporation" is a registered trademark.
-
+#
 # host: most Linux/GNU systems hosting the native toolchain.
+#
+# Adapted from kernel.org:linux-5.15.11/scripts/subarch.include .
 
 MACHINE				:=	$(shell uname -m)
-include $(DIMINUTO_ROOT)/cfg/arch.mk
+ARCH				:=	$(shell uname -m | sed -e 's/i.86/x86/' -e 's/x86_64/x86/' -e 's/sun4u/sparc64/' -e 's/arm.*/arm/' -e 's/sa110/arm/' -e 's/s390x/s390/' -e 's/ppc.*/powerpc/' -e 's/mips.*/mips/' -e 's/sh[234].*/sh/' -e 's/aarch64.*/arm64/' -e 's/riscv.*/riscv/')
 OS					:=	$(shell uname -o)
 TOOLCHAIN			:=
 KERNELCHAIN			:=
