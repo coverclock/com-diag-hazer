33c145c4a5ab09accb448c5da01fc0cfbee7853d
Chip Overclock <coverclock@diag.com>
2022-07-25 14:07:00 -0600

    [1] Added single and double precision output for ubxval. TESTED [2] Checkpointing zedf9t (still not working).

--- Hazer/bin/ubxval.c
+++ Hazer/bin/ubxval.c
@@ -12,11 +12,13 @@
  *
  * Converts a number in octal, decimal, or hexidecimal into
  * hexadecimal little endian form usable by the -U command line
- * flag in gpstool to send to a UBX-speaking device.
+ * flag in gpstool to send to a UBX-speaking device. A number can
+ * be a 1, 2, 4, or 8 byte integer, or a single (4) or double (8)
+ * precision floating point.
  *
  * USAGE
  *
- * ubxval [ -1 | -2 | -4 | -8 | +1 | +2 | +4 | +8 ] NUMBER
+ * ubxval [ -1 | -2 | -4 | -8 | -s | -d | +1 | +2 | +4 | +8 | +s | +d ] NUMBER
  */
 
 #include <stdio.h>
@@ -48,6 +50,8 @@ int main(int argc, char * argv[])
     uint32_t n4 = 0;
     uint16_t n2 = 0;
     uint8_t  n1 = 0;
+    float f4 = 0.0;
+    double f8 = 0.0;
     char * end = (char *)0;
     const char * name = (char *)0;
     int debug = 0;
@@ -73,7 +77,7 @@ int main(int argc, char * argv[])
         }
 
         if (argv[1][1] == '?') {
-            fprintf(stderr, "usage: %s [ -1 | -2 | -4 | -8 | +1 | +2 | +4 | +8 ] NUMBER\n", name);
+            fprintf(stderr, "usage: %s [ -1 | -2 | -4 | -8 | -s | -d | +1 | +2 | +4 | +8 | +s | +d ] NUMBER\n", name);
             xc = 0;
             break;
         }
@@ -84,36 +88,62 @@ int main(int argc, char * argv[])
             break;
         }
 
-        n8 = strtoll(argv[2], &end, 0);
-        if (end == argv[2]) {
-            errno = EINVAL;
-            perror(argv[2]);
-            break;
-        }
+        if (argv[1][1] == 's') {
 
-        size = atoi(&(argv[1][1]));
-        if (size == sizeof(n1)) {
-            n1 = n8;
-            if (debug) { fprintf(stderr, "%s: %02x\n", name, n1); }
-            emit(&n1, sizeof(n1));
-        } else if (size == sizeof(n2)) {
-            n2 = n8;
-            if (debug) { fprintf(stderr, "%s: %04x\n", name, n2); }
-            n2 = htole16(n2);
-            emit(&n2, sizeof(n2));
-        } else if (size == sizeof(n4)) {
-            n4 = n8;
-            if (debug) { fprintf(stderr, "%s: %08lx\n", name, (unsigned long)n4); }
-            n4 = htole32(n4);
-            emit(&n4, sizeof(n4));
-        } else if (size == sizeof(n8)) {
-            if (debug) { fprintf(stderr, "%s: %016llx\n", name, (unsigned long long)n8); }
-            n8 = htole64(n8);
-            emit(&n8, sizeof(n8));
+            size = sscanf(argv[2], "%f", &f4);
+            if (size != 1) {
+                errno = EINVAL;
+                perror(argv[2]);
+                break;
+            }
+            if (debug) { fprintf(stderr, "%s: %f\n", name, f4); }
+            emit(&f4, sizeof(f4));
+
+        } else if (argv[1][1] == 'd') {
+
+            size = sscanf(argv[2], "%lf", &f8);
+            if (size != 1) {
+                errno = EINVAL;
+                perror(argv[2]);
+                break;
+            }
+            if (debug) { fprintf(stderr, "%s: %lf\n", name, f8); }
+            emit(&f8, sizeof(f8));
+
         } else {
-            errno = EINVAL;
-            perror(argv[1]);
-            break;
+
+            n8 = strtoll(argv[2], &end, 0);
+            if (end == argv[2]) {
+                errno = EINVAL;
+                perror(argv[2]);
+                break;
+            }
+
+            size = atoi(&(argv[1][1]));
+            if (size == sizeof(n1)) {
+                n1 = n8;
+                if (debug) { fprintf(stderr, "%s: %02x\n", name, n1); }
+                emit(&n1, sizeof(n1));
+            } else if (size == sizeof(n2)) {
+                n2 = n8;
+                if (debug) { fprintf(stderr, "%s: %04x\n", name, n2); }
+                n2 = htole16(n2);
+                emit(&n2, sizeof(n2));
+            } else if (size == sizeof(n4)) {
+                n4 = n8;
+                if (debug) { fprintf(stderr, "%s: %08lx\n", name, (unsigned long)n4); }
+                n4 = htole32(n4);
+                emit(&n4, sizeof(n4));
+            } else if (size == sizeof(n8)) {
+                if (debug) { fprintf(stderr, "%s: %016llx\n", name, (unsigned long long)n8); }
+                n8 = htole64(n8);
+                emit(&n8, sizeof(n8));
+            } else {
+                errno = EINVAL;
+                perror(argv[1]);
+                break;
+            }
+
         }
 
         xc = 0;
--- Hazer/fun/zedf9t.sh
+++ Hazer/fun/zedf9t.sh
@@ -27,19 +27,36 @@ exec 2>>${ERRFIL}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-# Note: the UBX-CFG-TP5 message is deprecated, but I was unable to get the
-# UBX-CFG-VALSET equivalents to work on the ZED-F9T running FWVER 2.01
-# PROTVER 29.00. I noticed that the SparkFun 2.2.11 u-blox GNSS library for
-# Arduino still uses the deprecated UBX-CFG-TP5 message. (I haven't gotten
-# configuring Timepulse 1 (versus 2) working using either mechanism.)
-
 # UBX-MON-VER [0]
 
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TMODE-MODE SURVEY_IN
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TMODE-SVIN_MIN_DUR 300 (@sec = 5min)
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TMODE-SVIN_ACC_LIMIT 250 (@0.1mm = 2.5cm = ~1in)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TMODE-MODE SURVEY_IN
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TMODE-SVIN_MIN_DUR 300 (@sec = 5min)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TMODE-SVIN_ACC_LIMIT 250 (@0.1mm = 2.5cm = ~1in)
 
-# UBX-CFG-TP5 [32] 1 1 0x0000 0[2] 0[2] 10[4] 10[4] 5[4] 5[4] 0[4] 0x2077[4]
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-PULSE_DEF 1 (frequency)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-PULSE_LENGTH_DEF 0 (ratio)
+
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-FREQ_TP1 1 (Hz)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-FREQ_LOCK_TP1 1 (Hz)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-DUTY_TP1 0.5 (%)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-DUTY_LOCK_TP1 0.5 (%)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-TIMEGRID_TP1 1 (GPS)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-ALIGN_TO_TOW_TP1 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-USE_LOCKED_TP1 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-POL_TP1 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-TP1_ENA 1 ("Must be set for frequency-time products.")
+
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-FREQ_TP2 10 000 000 (Hz)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-FREQ_LOCK_TP2 10 000 000 (Hz)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-DUTY_TP2 0.5 (%)
+# UBX-CFG-VALSET [12] V0 Flash 0x0000 CFG-TP-DUTY_LOCK_TP2 0.5 (%)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-TIMEGRID_TP2 1 (GPS)
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-ALIGN_TO_TOW_TP2 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-USE_LOCKED_TP2 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-POL_TP2 1
+# UBX-CFG-VALSET [9] V0 Flash 0x0000 CFG-TP-TP2_ENA 1
+
+# UBX-CFG-RST [4] coldstart hardwareshutdownreset
 
 exec coreable gpstool \
     -H ${OUTFIL} -F 1 -t 10 \
@@ -48,11 +65,43 @@ exec coreable gpstool \
     -D ${DEVICE} -b ${RATE} -8 -n -1 \
     -x \
     -U '\xb5\x62\x0a\x04\x00\x00' \
-    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x01\x00\x03\x20'"$(ubxval -1 1)" \
-    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x10\x00\x03\x40'"$(ubxval -4 300)" \
-    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x11\x00\x03\x40'"$(ubxval -4 250)" \
-    -A '\xb5\x62\x06\x31'"$(ubxval -2 32)$(ubxval -1 1)$(ubxval -1 1)"'\x00\x00'"$(ubxval -2 0)$(ubxval -2 0)$(ubxval -4 10000000)$(ubxval -4 10000000)$(ubxval -4 5)$(ubxval -4 5)$(ubxval -4 0)$(ubxval -4 0x247f)" \
+    \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x01\x00\x01\x00\x03\x20'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x02\x00\x10\x00\x03\x40'"$(ubxval -4 300)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x03\x00\x11\x00\x03\x40'"$(ubxval -4 250)" \
+    \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x01\x00\x20\x05\x00\x23'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x20\x05\x00\x30'"$(ubxval -1 0)" \
+    \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x02\x00\x40\x05\x00\x24'"$(ubxval -4 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x02\x00\x40\x50\x00\x25'"$(ubxval -4 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 16)"'\x00\x04\x02\x00\x50\x05\x00\x2a'"$(ubxval -d 0.5)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 16)"'\x00\x04\x02\x00\x50\x05\x00\x2b'"$(ubxval -d 0.5)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x20\x05\x00\x0c'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x0a'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x09'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x0b'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x50\x05\x00\x2b'"$(ubxval -1 1)" \
+    \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x02\x00\x40\x05\x00\x26'"$(ubxval -4 10000000)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 12)"'\x00\x04\x02\x00\x50\x05\x00\x27'"$(ubxval -4 10000000)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 16)"'\x00\x04\x02\x00\x50\x05\x00\x2c'"$(ubxval -d 0.5)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2 16)"'\x00\x04\x02\x00\x50\x05\x00\x2d'"$(ubxval -d 0.5)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x20\x05\x00\x17'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x15'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x14'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x02\x00\x10\x05\x00\x16'"$(ubxval -1 1)" \
+    -A '\xb5\x62\x06\x8a'"$(ubxval -2  9)"'\x00\x04\x03\x00\x10\x05\x00\x12'"$(ubxval -1 1)" \
+    \
+    -U '\xb5\x62\x06\x04'"$(ubxval -2 4)"'\xff\xff\x04\x00' \
+    -U '' \
     < /dev/null 1> /dev/null
+
+#####
+
+# UBX-CFG-TP5 [32] 1 1 0x0000 0[2] 0[2] 10[4] 10[4] 5[4] 5[4] 0[4] 0x2077[4]
+
+#    -A '\xb5\x62\x06\x31'"$(ubxval -2 32)$(ubxval -1 1)$(ubxval -1 1)"'\x00\x00'"$(ubxval -2 0)$(ubxval -2 0)$(ubxval -4 10000000)$(ubxval -4 10000000)$(ubxval -4 5)$(ubxval -4 5)$(ubxval -4 0)$(ubxval -4 0x247f)" \
 
 #####
 
@@ -62,49 +111,3 @@ exec coreable gpstool \
 
 #####
 
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-PULSE_DEF 0 (period)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-PULSE_LENGTH_DEF 1 (length)
-
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-PERIOD_TP1 1 000 000 (us)
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-LEN_TP1 100 000 (us)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-TIMEGRID_TP1 1 (GPS)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-ALIGN_TO_TOW_TP1 1
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-USE_LOCKED_TP1 1
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-POL_TP1 1
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-PERIOD_LOCK_TP1 100 000 (us)
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-LEN_LOCK_TP1 100 000 (us)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-TP1_ENA 1 ("Must be set for frequency-time products.")
-
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-PERIOD_TP2 1 000 000 (us)
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-LEN_TP2 100 000 (us)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-TIMEGRID_TP2 1 (GPS)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-ALIGN_TO_TOW_TP2 0
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-USE_LOCKED_TP2 1
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-POL_TP2 1
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-PERIOD_LOCK_TP2 100 000 (us)
-# UBX-CFG-VALSET [12] V0 RAM 0x0000 CFG-TP-LEN_LOCK_TP2 100 000 (us)
-# UBX-CFG-VALSET [9] V0 RAM 0x0000 CFG-TP-TP2_ENA 1
-
-#    \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x20\x05\x00\x23'"$(ubxval -1 0)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x20\x05\x00\x30'"$(ubxval -1 1)" \
-#    \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x02'"$(ubxval -4 1000000)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x04'"$(ubxval -4 100000)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x20\x05\x00\x0c'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x0a'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x09'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x0b'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x03'"$(ubxval -4 1000000)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x05'"$(ubxval -4 100000)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x07'"$(ubxval -1 1)" \
-#    \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x0d'"$(ubxval -4 1000000)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x0f'"$(ubxval -4 100000)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x20\x05\x00\x17'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x15'"$(ubxval -1 0)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x14'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x16'"$(ubxval -1 1)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x03'"$(ubxval -4 1000000)" \
-#    -A '\xb5\x62\x06\x8a\x0c\x00\x00\x01\x00\x00\x40\x05\x00\x10'"$(ubxval -4 100000)" \
-#    -A '\xb5\x62\x06\x8a\x09\x00\x00\x01\x00\x00\x10\x05\x00\x12'"$(ubxval -1 1)" \
