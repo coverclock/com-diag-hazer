5e8cc2c7bb6859a937fdb28ea758c287a277b873
Chip Overclock <coverclock@diag.com>
2022-07-29 09:05:59 -0600

    [1] Checkpointing Stagecoach work while debugging.

--- Hazer/bin/tunnel.sh
+++ Hazer/bin/tunnel.sh
@@ -6,8 +6,6 @@
 # WORK IN PROGRESS!
 
 SSLDIR=${COM_DIAG_HAZER_SSLDIR:-$(readlink -e $(dirname ${0})/..)/ssl}
-SAVDIR=${COM_DIAG_HAZER_SAVDIR:-$(readlink -e $(dirname ${0})/..)/tmp}
-mkdir -p ${SAVDIR}
 
 PROGRAM=$(basename ${0})
 ROLE=${1:-""}
@@ -16,30 +14,31 @@ if [[ "${ROLE}" == "rover" ]]; then
 	HOST=${3:-"localhost"}
 	SINK=${4:-"stagecoach"}
 	PEMFIL=${5:-"${SSLDIR}/rover.pem"}
-	CRTFIL=${6:-"${SSLDIR}/rover.crt"}
-	COMMON=${7:-"stagecoach"}
+	CRTFIL=${6:-"${SSLDIR}/base.crt"}
+	COMMON=${7:-"rover.prariethorn.org"}
 elif [[ "${ROLE}" == "base" ]]; then
 	HOST=${2:-"localhost"}
 	SOURCE=${3:-"stagecoach"}
 	SINK=${4:-"tumbleweed"}
 	PEMFIL=${5:-"${SSLDIR}/base.pem"}
 	CRTFIL=${6:-"${SSLDIR}/rover.crt"}
-	COMMON=${7:-"stagecoach"}
+	COMMON=${7:-"base.prariethorn.org"}
 else
+	echo ${PROGRAM}: ${ROLE}? 1>&2
 	exit 1
 fi
-ERRFIL=${8:-"${SAVDIR}/${PROGRAM}.err"}
-
-mkdir -p $(dirname ${ERRFIL})
-cp /dev/null ${ERRFIL}
-exec 2>>${ERRFIL}
-
-. $(readlink -e $(dirname ${0})/../bin)/setup
 
 if [[ "${ROLE}" == "rover" ]]; then
-	exec socat UDP-DATAGRAM:localhost:${SOURCE} openssl-connect:${HOST}:${SINK},openssl-commonname=${COMMON},cert=${PEMFIL},cafile=${CRTFIL}
+	COMMAND="socat UDP-DATAGRAM:localhost:${SOURCE} openssl-connect:${HOST}:${SINK},openssl-commonname=${COMMON},cert=${PEMFIL},cafile=${CRTFIL}"
+	echo ${PROGRAM}: ${COMMAND} 1>&2
+	${COMMAND}
 elif [[ "${ROLE}" == "base" ]]; then
-	exec socat openssl-listen:${SOURCE},reuseaddr,fork,cert=${PEMFIL},cafile=${CRTFIL},verify=1 UDP-DATAGRAM:${HOST}:${SINK}
+	COMMAND="socat openssl-listen:${SOURCE},reuseaddr,fork,cert=${PEMFIL},cafile=${CRTFIL},verify=1 UDP-DATAGRAM:${HOST}:${SINK}"
+	echo ${PROGRAM}: ${COMMAND} 1>&2
+	${COMMAND}
 else
-	exit 2
+	echo ${PROGRAM}: ${ROLE}! 1>&2
+	exit 4
 fi
+
+exit 0
