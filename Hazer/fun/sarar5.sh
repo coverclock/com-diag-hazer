ad2292c85f4b759b16415b2877482f018ed2c3ec
Chip Overclock <coverclock@diag.com>
2022-11-24 10:02:36 -0700

    [1] Fixed stupid bug re: FIFOs. TESTED

--- Hazer/fun/sarar5.sh
+++ Hazer/fun/sarar5.sh
@@ -6,8 +6,8 @@
 # THIS IS A WORK IN PROGRESS.
 # Test the U-blox SARA-R5 on the SparkFun board. The SARA-R5
 # combines an LTE-M radio with a M8 GNSS receiver.
-# Usage: sarar5 [ DEVICE [ RATE [ ERRFIL [ OUTFIL [ CSVFIO [ PIDFIL ] ] ] ] ] ]
-# Defaults: sarar5 /dev/ttyUSB0 115200 out/host/tmp/sarar5.err out/host/tmp/sarar5.out out/host/tmp/sarar5.csv out/host/tmp/sarar5.pid
+# Usage: sarar5 [ DEVICE [ RATE [ HOST [ PORT [ ERRFIL [ OUTFIL [ CSVFIO [ PIDFIL ] ] ] ] ] ] ] ]
+# Defaults: sarar5 /dev/ttyUSB0 115200 localhost 23030 out/host/tmp/...
 
 SAVDIR=${COM_DIAG_HAZER_SAVDIR:-$(readlink -e $(dirname ${0})/..)/tmp}
 mkdir -p ${SAVDIR}
@@ -15,28 +15,27 @@ mkdir -p ${SAVDIR}
 PROGRAM=$(basename -s .sh ${0})
 DEVICE=${1:-"/dev/ttyUSB0"}
 RATE=${2:-115200}
-ERRFIL=${3-"${SAVDIR}/${PROGRAM}.err"}
-OUTFIL=${4-"${SAVDIR}/${PROGRAM}.out"}
-CSVFIO=${5-"${SAVDIR}/${PROGRAM}.csv"}
-PIDFIL=${6-"${SAVDIR}/${PROGRAM}.pid"}
+HOST=${3:-"localhost"}
+PORT=${4:-"23030"}
+ERRFIL=${5-"${SAVDIR}/${PROGRAM}.err"}
+OUTFIL=${6-"${SAVDIR}/${PROGRAM}.out"}
+PIDFIL=${7-"${SAVDIR}/${PROGRAM}.pid"}
+CSVFIO=${8-"${SAVDIR}/${PROGRAM}.fio"}
 
 mkdir -p $(dirname ${ERRFIL})
 mkdir -p $(dirname ${OUTFIL})
-mkdir -p $(dirname ${CSVFIO})
 mkdir -p $(dirname ${PIDFIL})
+mkdir -p $(dirname ${CSVFIO})
 
 cp /dev/null ${ERRFIL}
 exec 2>>${ERRFIL}
 
 rm -f ${CSVFIO}
 mkfifo -m 600 ${CSVFIO}
-trap "rm -f ${CSVFIO}" SIGINT SIGQUIT SIGTERM EXIT
-# socat -u PIPE:${CSVFIO} - &
+socat -u PIPE:${CSVFIO} UDP-DATAGRAM:${HOST}:${PORT} & CATPID=$!
+# socat -u UDP-RECV:${PORT} -
+trap "kill ${CATPID}; rm -f ${CSVFIO}" SIGINT SIGQUIT SIGTERM EXIT
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
-exec coreable gpstool \
-    -D ${DEVICE} -b ${RATE} -8 -n -1 -n -1 \
-    -H ${OUTFIL} -F 1 -t 10 \
-    -T ${CSVFIO} \
-    -O ${PIDFIL}
+exec gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -H ${OUTFIL} -F 1 -t 10 -T ${CSVFIO} -O ${PIDFIL} < /dev/null
