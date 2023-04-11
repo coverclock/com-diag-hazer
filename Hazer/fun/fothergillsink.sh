da1391320f4258d0442f23adeade093829c2280a
Chip Overclock <coverclock@diag.com>
2023-04-11 06:58:56 -0600

    [1] Added a fothergill sink script. UNTESTED

---
+++ Hazer/fun/fothergillsink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2023 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+# In support of testing the Fothergill project, in which gpstool
+# forwards CSV-based traces over LoRa radios using HDLC-like
+# framing.
+
+RADDEV=${1:-"/dev/ttyACM0"}
+RADBPS=${2:-57600}
+
+exec framertool -D ${RADDEV} -b ${RADBPS} -8 -n -1 -d
