15205480872c8dfab94424d2f5e420e35767186e
Chip Overclock <coverclock@diag.com>
2023-07-17 08:19:13 -0600

    [1] Comments. UNTESTED

--- Hazer/fun/neof10tH01.sh
+++ Hazer/fun/neof10tH01.sh
@@ -10,6 +10,7 @@
 # u-blox device I've used. N.B. the labeling of the PVT
 # (1PPS) and POWER LEDs are reversed on this version
 # of the SimpleGNSS board.
+# WORK IN PROGRESS (doesn't currently work)
 
 PROGRAM=$(basename ${0})
 DEVICE=${1:-"/dev/ttyUSB0"}
