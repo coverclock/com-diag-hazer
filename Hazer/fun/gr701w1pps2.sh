6ee7f7170eb9347166a878a159b99f98041cd1cf
Chip Overclock <coverclock@diag.com>
2023-07-25 10:57:25 -0600

    [1] Added example of UBX-RXM-RAWX data from UBX-NEO-F10T. [2] Added script for GR701W that does 1PPS but not STROBE. UNTESTED

---
+++ Hazer/fun/gr701w1pps2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2023 Digital Aggregates Corporation, Colorado, USA
+# Licensed under the terms in LICENSE.txt
+# Chip Overclock <coverclock@diag.com>
+# https://github.com/coverclock/com-diag-hazer
+#
+# This script tests the NaviSys Technology GR-701W, a USB GPS
+# device that is based on a Ublox 7 receiver. The GR-701W is
+# unusual amongst GPS devices that have a USB interface in that
+# it exports the 1PPS signal via data carrier detect (DCD), which
+# is the reason for the "-c" option below.
+
+PROGRAM=$(basename ${0})
+DEVICE=${1:-"/dev/ttyUSB0"}
+RATE=${2:-9600}
+
+. $(readlink -e $(dirname ${0})/../bin)/setup
+
+DIR=$(readlink -e $(dirname ${0})/..)/log
+mkdir -p ${DIR}
+
+coreable gpstool -D ${DEVICE} -b ${RATE} -8 -n -1 -c -E -t 10 2>> ${DIR}/${PROGRAM}.err
