3c495027b5cdfb847dc962af55ed9eebe35bbe88
Chip Overclock <coverclock@diag.com>
2023-08-16 09:58:29 -0600

    [1] Checkpoint while debugging.

--- Hazer/fun/zedf9p.sh
+++ Hazer/fun/zedf9p.sh
@@ -8,10 +8,13 @@
 SAVDIR=${COM_DIAG_HAZER_SAVDIR:-$(readlink -e $(dirname ${0})/..)/tmp}
 
 PGMNAM=$(basename ${0})
+FILNAM=${PGMNAM%%-*}
 LOCDEV=${1:-"/dev/ttyACM0"}
 LOCBPS=${2:-38400}
-ERRFIL=${3:-"${SAVDIR}/${PGMNAM}.err"}
-OUTFIL=${4:-"${SAVDIR}/${PGMNAM}.out"}
+CORDEV=${3:-"/dev/ttyACM1"}
+CORBPS=${4:-9600}
+ERRFIL=${5:-"${SAVDIR}/${FILNAM}.err"}
+OUTFIL=${6:-"${SAVDIR}/${PGMNAM}.out"}
 
 . $(readlink -e $(dirname ${0})/../bin)/setup
 
@@ -21,13 +24,37 @@ mkdir -p $(dirname ${OUTFIL})
 #cp /dev/null ${ERRFIL}
 exec 2>>${ERRFIL}
 
-# UBX-CFG-MSG [ 3] UBX-NAV-HPPOSLLH=1
+#####
+# QUERY THE UBX-NEO-D9S INMARSAT RECEIVER.
+#####
+
+MESSAGE="${PGMNAM}: Querying UBX-NEO-D9S ${CORDEV} ${CORBPS}"
+log -I -N ${PGMNAM} -n "${MESSAGE}"
+echo "${MESSAGE}"
+
 # UBX-MON-TXBUF [ 0]
 
-eval gpstool \
+gpstool \
+    -R \
+    -D ${CORDEV} -b ${CORBPS} -8 -n -1 \
+    -w 5 -x \
+    -U '\xb5\x62\x0a\x08\x00\x00' \
+    -U '' \
+    < /dev/null
+
+# UBX-CFG-MSG [ 3] UBX-NAV-HPPOSLLH=1
+
+#####
+# PROCESS THE UBX-ZED-F9P GNSS RECEIVER.
+#####
+
+MESSAGE="${PGMNAM}: Processing UBX-ZED-F9P ${LOCDEV} ${LOCBPS}"
+log -I -N ${PGMNAM} -n "${MESSAGE}"
+echo "${MESSAGE}"
+
+exec gpstool \
     -H ${OUTFIL} -F 1 -t 10 \
     -D ${LOCDEV} -b ${LOCBPS} -8 -n -1 \
     -w 5 -x \
-    -A '\\xb5\\x62\\x06\\x01\\x03\\x00\\x01\\x14\\x01' \
-    -U '\\xb5\\x62\\x0a\\x08\\x00\\x00' \
+    -A '\xb5\x62\x06\x01\x03\x00\x01\x14\x01' \
     < /dev/null
