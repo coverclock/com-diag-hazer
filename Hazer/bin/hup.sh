2d9c8872f5096dba33b7fc107a39625adcac549d
Chip Overclock <coverclock@diag.com>
2023-08-31 06:03:12 -0600

    [1] Corrected comments in hup scripts. TESTED

--- Hazer/bin/hup.sh
+++ Hazer/bin/hup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Copyright 2020 Digital Aggregates Corporation, Colorado, USA
+# Copyright 2020-2023 Digital Aggregates Corporation, Colorado, USA
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
-# USAGE hup [ PROGRAM [ SIGNAL ] ]
-# EXAMPLE hup
-# EXAMPLE hup INT
-# EXAMPLE hup INT footool
 # Send a SIGHUP to all gpstool instances if any.
+# USAGE hup [ PROGRAM [ SIGNAL ] ]
+# EXAMPLES
+#    hup
+#    hup gpstool
+#    hup gpstool HUP
 
 exec pkill -${2:-"HUP"} ${1:-"gpstool"}
--- Hazer/bin/hups.sh
+++ Hazer/bin/hups.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-# Copyright 2020 Digital Aggregates Corporation, Colorado, USA
+# Copyright 2020-2023 Digital Aggregates Corporation, Colorado, USA
 # Licensed under the terms in LICENSE.txt
 # Chip Overclock <coverclock@diag.com>
 # https://github.com/coverclock/com-diag-hazer
 # Send a SIGHUP to all gpstool instances if any whenever
 # user presses any key.
+# USAGE hups [ PROGRAM [ SIGNAL ] ]
+# EXAMPLES
+#    hups
+#    hups gpstool
+#    hups gpstool HUP
 
-PID=${1:-$$}
+PGM=${1:-"gpstool"}
 SIG=${2:-"HUP"}
 while read -p "${SIG}? " -N 1 INPUT; do
-	kill -${SIG} ${PID}
+    pkill -${SIG} ${PGM}
 done
 echo
 exit 0
