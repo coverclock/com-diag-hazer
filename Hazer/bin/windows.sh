7d6874c058b6f276eed7e7cf9445212b4889d2a1
Chip Overclock <coverclock@diag.com>
2023-09-03 11:10:19 -0600

    [1] Experimenting with xterminal emulators. UNTESTED

--- Hazer/bin/windows.sh
+++ Hazer/bin/windows.sh
@@ -12,10 +12,23 @@
 BINDIR=$(readlink -e $(dirname ${0})/../bin)
 SAVDIR=${COM_DIAG_HAZER_SAVDIR:-${BINDIR}/../tmp}
 
+PGMNAM=$(basename ${0})
 APPNAM=${1:-"spartan"}
 BASNAM=${2:-"spartan"}
 
-TERMINAL="xfce4-terminal"
+if true; then
+    TERMINAL="x-terminal-emulator"
+elif false; then
+    TERMINAL="xfce4-terminal"
+elif false; then
+    TERMINAL="gnome-terminal"
+elif false; then
+    TERMINAL="mate-terminal"
+elif false; then
+    TERMINAL="lxterminal"
+else
+    TERMINAL="xterm"
+fi
 
 mkdir -p ${SAVDIR}
 touch ${SAVDIR}/${BASNAM}.csv
